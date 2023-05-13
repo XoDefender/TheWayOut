2ec2fa7844fb5fad481c2a5f29900e3fcd2cd0e5
denfranke <33222627+denfranke@users.noreply.github.com>
2023-05-13 12:41:24 +0300

    Добавлены кнопки

---
+++ Assets/Scripts/UI/ButtonsFunctions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsFunctions : MonoBehaviour
+{
+    private int LevelCount = 2;
+    public void ReastartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void NextLevel()
+    {
+        //Debug.Log(SceneManager.GetActiveScene().buildIndex);
+        if (SceneManager.GetActiveScene().buildIndex - 1 <= LevelCount)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        else
+            SceneManager.LoadScene(0);
+    }
+}
