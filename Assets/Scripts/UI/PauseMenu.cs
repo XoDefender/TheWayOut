c7a0a2f31251180ad4d14169bab5cde8593f22f8
denfranke <33222627+denfranke@users.noreply.github.com>
2023-05-16 00:45:50 +0300

    добавил рабочий интерфейс паузы
    (не останавливает время)

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool active;
+    private void Start()
     {
-        
+        Pause_Menu.gameObject.SetActive(false);
+        active = false;
     }
-
+    public GameObject Pause_Menu;
     // Update is called once per frame
     void Update()
     {
-        
+          ESC();
+    }
+    public void ESC()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+            Debug.Log("+\n");
+            int d = 0;
+            for (long i = 0; i<500000000; i++)
+                d= 0;
+            active=!active;
+            Pause_Menu.gameObject.SetActive(active);
+
+        }
+    }
+
+    public void pause()
+    {
+        active=!active;
+        Pause_Menu.gameObject.SetActive(active);
     }
 }
